cde6a3f8a7bb06e86a9dc851be054e6d2226d13a
Sophe Mae Dela Cruz <scdelacruz6@up.edu.ph>
2025-10-31 22:38:01 +0800

    Added Header and Footer on Scholarships page
    Added header and footer on scholarships page and modified the routes for the webpages.

---
+++ osapp-frontend/src/components/Header.jsx
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom'
+import { FaChevronDown } from 'react-icons/fa'
+import upvLogo from '../assets/upvlogo.png'
+import '../styles/Header.css'
+
+function Header() {
+    return(
+        <header className='header'>
+            <Logo />
+
+            <nav className='roboto-header-right'>
+                <Link to='/'>Home</Link>
+
+                <div className='dropdown'>
+                    <button className='dropBtn'>
+                        Downloads <FaChevronDown className="chevronDown"/>
+                    </button>
+                </div>
+                <Link to="/login">Login</Link>
+                <Link to="/signup" className='signupBtn'>Sign Up</Link>
+            </nav>
+
+        </header>
+    )
+}
+
+function Logo() {
+    return(
+        <div className='headerLeft'>
+            <div className='logo'>
+                <img src={ upvLogo } alt='UPV Logo' className='upvLogo' />
+            </div>
+            
+            <div className='source-serif-4-header'>
+                <h2>UPV Office of Student Affairs</h2>
+                <h3>Scholarships and Financial Assistance Unit</h3>
+            </div>
+        </div>
+    )
+}
+export default Header
